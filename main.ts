1ae903c0c546d0a88d4de3c58f425d6e0a9dcc3d
tiilt-lab-code <60939621+tiilt-lab-code@users.noreply.github.com>
2026-02-10 06:26:56 -0600

    Added a timer and ability to count passes on the coach device

--- main.ts
+++ main.ts
@@ -10,11 +10,17 @@ radio.onReceivedNumber(function (receivedNumber) {
             `)
     }
 })
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    radio.setGroup(11)
+    basic.showString("11")
+})
 input.onButtonPressed(Button.A, function () {
     if (!(running) && !(coach_mode)) {
         radio.sendString("add")
     } else if (!(running) && coach_mode) {
         running = true
+        start_time = input.runningTime()
+        passes = 0
         basic.showIcon(IconNames.Heart)
     }
 })
@@ -22,7 +28,7 @@ input.onButtonPressed(Button.AB, function () {
     if (!(running) && !(coach_mode)) {
         coach_mode = true
         players = []
-        basic.showString("Coach")
+        basic.showString("C")
     }
 })
 radio.onReceivedString(function (receivedString) {
@@ -35,16 +41,22 @@ radio.onReceivedString(function (receivedString) {
 })
 input.onButtonPressed(Button.B, function () {
     if (coach_mode) {
-        delay += -1
-        basic.showNumber(delay)
-        basic.pause(1000)
+        if (1 < delay) {
+            delay += -1
+            basic.showNumber(delay)
+            basic.pause(1000)
+        }
     }
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    radio.setGroup(11)
-    basic.showString("group 11")
+    if (coach_mode && running) {
+        passes += 1
+        basic.showNumber(passes)
+    }
 })
 let players: number[] = []
+let passes = 0
+let start_time = 0
 let delay = 0
 let running = false
 let coach_mode = false
@@ -52,13 +64,27 @@ coach_mode = false
 running = false
 radio.setGroup(13)
 radio.setTransmitSerialNumber(true)
-delay = 1
+delay = 10
+start_time = 0
+let game_duration = 30000
+passes = 0
+loops.everyInterval(1000, function () {
+    if (coach_mode) {
+        if (running) {
+            if (input.runningTime() - start_time >= game_duration) {
+                running = false
+                music.play(music.builtinPlayableSoundEffect(soundExpression.slide), music.PlaybackMode.UntilDone)
+                basic.showNumber(passes)
+            }
+        }
+    }
+})
 basic.forever(function () {
     if (running) {
         radio.sendNumber(players._pickRandom())
         basic.showIcon(IconNames.Heart)
         for (let index = 0; index < delay; index++) {
-            basic.pause(1000)
+            basic.pause(500)
         }
         basic.showIcon(IconNames.SmallHeart)
     }
